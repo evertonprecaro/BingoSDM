078b1edfd7c0b6fdd004c523e4bab1836c89d326
evertonprecaro <42481696+evertonprecaro@users.noreply.github.com>
2018-09-09 14:00:59 -0300

    Mudança de main activity para linear layout.

--- app/src/main/java/com/example/evertonprecaro/dicesdm/MainActivity.java
+++ app/src/main/java/com/example/evertonprecaro/dicesdm/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear_layout_activity_main);
 
 
         //Recuperando referencia para os componesntes de Layout
